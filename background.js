e58a6ada7591ce42b448fa85550026d309a39114
Raikungfu <raikungfu@gmail.com>
2024-08-31 13:40:13 +0700

    new

--- background.js
+++ background.js
@@ -1,160 +1,202 @@
 function startAutoClickVote(maxClicks, delayBetweenClicks, breakDuration) {
-    const browserApi = typeof browser !== 'undefined' ? browser : chrome;
-    let isFirstRun = true;
-    browserApi.storage.local.get('autoClickRunning', (data) => {
-        if (data.autoClickRunning) return;
-        browserApi.storage.local.set({ autoClickRunning: true });
-    });
-    
-    browserApi.storage.local.get(['clickCount', 'totalClickCount', 'captchaMissCount'], (data) => {
-        let clickCount = data.clickCount || 0;
-        let captchaMissCount = data.captchaMissCount || 0;
-        let totalClickCount = data.totalClickCount || 0;
-        console.log("click count: " + clickCount);
+  const browserApi = typeof browser !== "undefined" ? browser : chrome;
+  let isFirstRun = true;
+  browserApi.storage.local.get("autoClickRunning", (data) => {
+    if (data.autoClickRunning) return;
+    browserApi.storage.local.set({ autoClickRunning: true });
+  });
 
-        function autoClick() {
-            try {
-                if (clickCount >= maxClicks || captchaMissCount > 5) {
-                    console.log("Waiting for 1 minute");
-                    console.log("Total vote: " + totalClickCount);
-                    browserApi.storage.local.get('intervalId', (data2) => {
-                        if (data2.intervalId) {
-                            clearInterval(data2.intervalId);
-                        }
+  browserApi.storage.local.get(
+    ["clickCount", "totalClickCount", "captchaMissCount"],
+    (data) => {
+      let clickCount = data.clickCount || 0;
+      let captchaMissCount = data.captchaMissCount || 0;
+      let totalClickCount = data.totalClickCount || 0;
+      console.log("click count: " + clickCount);
+
+      function autoClick() {
+        try {
+          if (clickCount >= maxClicks || captchaMissCount > 5) {
+            console.log("Waiting for 1 minute");
+            console.log("Total vote: " + totalClickCount);
+            browserApi.storage.local.get("intervalId", (data2) => {
+              if (data2.intervalId) {
+                clearInterval(data2.intervalId);
+              }
+            });
+            setTimeout(() => {
+              clickCount = 0;
+              captchaMissCount = 0;
+              browserApi.storage.local.set({ clickCount, captchaMissCount });
+              isFirstRun = true;
+              let actualDelay = delayBetweenClicks * 2;
+              let intervalId = setInterval(autoClick, actualDelay);
+              browserApi.storage.local.set({ intervalId });
+            }, breakDuration);
+            return;
+          }
+
+          const radioButton = document.querySelector(
+            'input[type="radio"][value="63529358"]'
+          );
+          const voteButton = document.getElementById("pd-vote-button14276245");
+
+          if (radioButton && voteButton) {
+            radioButton.click();
+            voteButton.click();
+
+            const checkInterval = 100;
+            let maxAttempts = isFirstRun ? 38 : 18;
+            isFirstRun = false;
+            let attempts = 0;
+
+            const intervalId1 = setInterval(() => {
+              const captchaElement = document.querySelector(".h-captcha p");
+              if (captchaElement) {
+                clearInterval(intervalId1);
+
+                const captchaText = captchaElement.textContent;
+                const [operand1, operand2] = captchaText
+                  .match(/\d+/g)
+                  .map(Number);
+                const result = operand1 + operand2;
+
+                const answerInput = document.querySelector(
+                  '.h-captcha input[type="text"]'
+                );
+                if (answerInput) {
+                  answerInput.value = result;
+                  const submitButton = document.getElementById(
+                    "pd-vote-button14276245"
+                  );
+                  if (submitButton) {
+                    ++totalClickCount;
+                    ++clickCount;
+                    browserApi.storage.local.set({
+                      totalClickCount,
+                      clickCount,
                     });
-                    setTimeout(() => {
-                        clickCount = 0;
-                        captchaMissCount = 0;
-                        browserApi.storage.local.set({ clickCount, captchaMissCount });
-                        isFirstRun = true;
-                        let actualDelay = delayBetweenClicks * 2;
-                        let intervalId = setInterval(autoClick, actualDelay);
-                        browserApi.storage.local.set({ intervalId });
-                    }, breakDuration);
-                    return;
+                    submitButton.click();
+                    new Promise((resolve) => {
+                      const observer = new MutationObserver((mutations) => {
+                        mutations.forEach((mutation) => {
+                          if (mutation.type === "childList") {
+                            const captchaResponse =
+                              document.querySelector(".pds-question-top");
+                            if (
+                              captchaResponse &&
+                              captchaResponse.textContent.includes(
+                                "Thank you for voting!"
+                              )
+                            ) {
+                              observer.disconnect();
+                              resolve();
+                            }
+                          }
+                        });
+                      });
+
+                      observer.observe(document.body, {
+                        childList: true,
+                        subtree: true,
+                      });
+
+                      setTimeout(() => {
+                        observer.disconnect();
+                        resolve();
+                      }, 1800);
+                    })
+                      .then(() => {
+                        console.log("Captcha submitted, reloading page...");
+                        location.reload();
+                      })
+                      .catch((error) => {
+                        console.error(
+                          "Error while waiting for captcha submission:",
+                          error
+                        );
+                      });
+                  }
                 }
-
-                const radioButton = document.querySelector('input[type="radio"][value="63080042"]');
-                const voteButton = document.getElementById('pd-vote-button14165530');
-
-                if (radioButton && voteButton) {
-                    radioButton.click();
-                    voteButton.click();
-
-                    const checkInterval = 100;
-                    let maxAttempts = isFirstRun ? 38 : 18;
-                    isFirstRun = false;
-                    let attempts = 0;
-
-                    const intervalId1 = setInterval(() => {
-                        const captchaElement = document.querySelector('.h-captcha p');
-                        if (captchaElement) {
-                            clearInterval(intervalId1);
-
-                            const captchaText = captchaElement.textContent;
-                            const [operand1, operand2] = captchaText.match(/\d+/g).map(Number);
-                            const result = operand1 + operand2;
-
-                            const answerInput = document.querySelector('.h-captcha input[type="text"]');
-                            if (answerInput) {
-                                answerInput.value = result;
-                                const submitButton = document.getElementById('pd-vote-button14165530');
-                                if (submitButton) {
-                                    ++totalClickCount;
-                                    ++clickCount;
-                                    browserApi.storage.local.set({ totalClickCount, clickCount });
-                                    submitButton.click();
-                                    new Promise((resolve) => {
-                                        const observer = new MutationObserver((mutations) => {
-                                            mutations.forEach((mutation) => {
-                                                if (mutation.type === 'childList') {
-                                                    const captchaResponse = document.querySelector('.pds-question-top');
-                                                    if (captchaResponse && captchaResponse.textContent.includes('Thank you for voting!')) {
-                                                        observer.disconnect();
-                                                        resolve();
-                                                    }
-                                                }
-                                            });
-                                        });
-
-                                        observer.observe(document.body, { childList: true, subtree: true });
-
-                                        setTimeout(() => {
-                                            observer.disconnect();
-                                            resolve();
-                                        }, 1800);
-                                    }).then(() => {
-                                        console.log('Captcha submitted, reloading page...');
-                                        location.reload();
-                                    }).catch((error) => {
-                                        console.error('Error while waiting for captcha submission:', error);
-                                    });
-                                }
-                            }
-                        } else {
-                            ++attempts;
-                            if (attempts >= maxAttempts) {
-                                clearInterval(intervalId1);
-                                console.log('Captcha not found after maximum attempts');
-                                ++captchaMissCount;
-                                browserApi.storage.local.set({ captchaMissCount });
-                                console.log('miss captcha: ' + captchaMissCount);
-                                new Promise((resolve) => {
-                                    const observer = new MutationObserver((mutations) => {
-                                        mutations.forEach((mutation) => {
-                                            if (mutation.type === 'childList') {
-                                                const captchaResponse = document.querySelector('.pds-question-top');
-                                                if (captchaResponse && captchaResponse.textContent.includes('Thank you for voting!')) {
-                                                    observer.disconnect();
-                                                    resolve();
-                                                }
-                                            }
-                                        });
-                                    });
-
-                                    observer.observe(document.body, { childList: true, subtree: true });
-
-                                    setTimeout(() => {
-                                        observer.disconnect();
-                                        resolve();
-                                    }, 1800);
-                                }).then(() => {
-                                    console.log('Captcha submitted, reloading page...');
-                                    location.reload();
-                                }).catch((error) => {
-                                    console.error('Error while waiting for captcha submission:', error);
-                                });
-                            }
+              } else {
+                ++attempts;
+                if (attempts >= maxAttempts) {
+                  clearInterval(intervalId1);
+                  console.log("Captcha not found after maximum attempts");
+                  ++captchaMissCount;
+                  browserApi.storage.local.set({ captchaMissCount });
+                  console.log("miss captcha: " + captchaMissCount);
+                  new Promise((resolve) => {
+                    const observer = new MutationObserver((mutations) => {
+                      mutations.forEach((mutation) => {
+                        if (mutation.type === "childList") {
+                          const captchaResponse =
+                            document.querySelector(".pds-question-top");
+                          if (
+                            captchaResponse &&
+                            captchaResponse.textContent.includes(
+                              "Thank you for voting!"
+                            )
+                          ) {
+                            observer.disconnect();
+                            resolve();
+                          }
                         }
-                    }, checkInterval);
+                      });
+                    });
 
-                } else {
-                    console.log('Radio button or vote button not found');
+                    observer.observe(document.body, {
+                      childList: true,
+                      subtree: true,
+                    });
+
+                    setTimeout(() => {
+                      observer.disconnect();
+                      resolve();
+                    }, 1800);
+                  })
+                    .then(() => {
+                      console.log("Captcha submitted, reloading page...");
+                      location.reload();
+                    })
+                    .catch((error) => {
+                      console.error(
+                        "Error while waiting for captcha submission:",
+                        error
+                      );
+                    });
                 }
-            } catch (error) {
-                console.error('Error detected, reloading page...', error);
-                location.reload();
-            }
+              }
+            }, checkInterval);
+          } else {
+            console.log("Radio button or vote button not found");
+          }
+        } catch (error) {
+          console.error("Error detected, reloading page...", error);
+          location.reload();
         }
+      }
 
-        browserApi.storage.local.get('intervalId', (data) => {
-            if (data.intervalId) {
-                clearInterval(data.intervalId);
-            }
-            let actualDelay = isFirstRun ? delayBetweenClicks * 2 : delayBetweenClicks;
-            let intervalId = setInterval(autoClick, actualDelay);
-            browserApi.storage.local.set({ intervalId });
-        });
-    });
+      browserApi.storage.local.get("intervalId", (data) => {
+        if (data.intervalId) {
+          clearInterval(data.intervalId);
+        }
+        let actualDelay = isFirstRun
+          ? delayBetweenClicks * 2
+          : delayBetweenClicks;
+        let intervalId = setInterval(autoClick, actualDelay);
+        browserApi.storage.local.set({ intervalId });
+      });
+    }
+  );
 }
 
-
 function stopAutoClickVote() {
-    const browserApi = typeof browser !== 'undefined' ? browser : chrome;
-    browserApi.storage.local.set({ autoClickRunning: false });
-    browserApi.storage.local.get('intervalId', (data) => {
-        if (data.intervalId) {
-            clearInterval(data.intervalId);
-        }
-    });
+  const browserApi = typeof browser !== "undefined" ? browser : chrome;
+  browserApi.storage.local.set({ autoClickRunning: false });
+  browserApi.storage.local.get("intervalId", (data) => {
+    if (data.intervalId) {
+      clearInterval(data.intervalId);
+    }
+  });
 }
--- popup.js
+++ popup.js
@@ -1,82 +1,101 @@
 function getBrowserApi() {
-    return typeof browser !== 'undefined' ? browser : chrome;
+  return typeof browser !== "undefined" ? browser : chrome;
 }
 
 const browserApi = getBrowserApi();
 
-document.addEventListener('DOMContentLoaded', () => {
-    browserApi.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        browserApi.scripting.executeScript({
-            target: { tabId: tabs[0].id },
-            function: checkAutoClickStatus
-        }, (results) => {
-            if (browserApi.runtime.lastError || !results || !results[0].result) {
-                // Nếu có lỗi hoặc không thể lấy kết quả, mặc định là "chưa chạy"
-                document.getElementById('start-auto-click').style.display = 'inline';
-                document.getElementById('stop-auto-click').style.display = 'none';
-            } else {
-                // Cập nhật nút dựa trên trạng thái của autoClickRunning
-                if (results[0].result) {
-                    document.getElementById('start-auto-click').style.display = 'none';
-                    document.getElementById('stop-auto-click').style.display = 'inline';
-                } else {
-                    document.getElementById('start-auto-click').style.display = 'inline';
-                    document.getElementById('stop-auto-click').style.display = 'none';
-                }
-            }
-        });
-    });
+// background.js
+browserApi.webRequest.onCompleted.addListener(
+  function (details) {
+    // Kiểm tra URL và mã phản hồi để xác định nếu có alert
+    if (details.url.includes("vote-js.php") && details.statusCode === 200) {
+      chrome.tabs.sendMessage(details.tabId, { type: "checkAlert" });
+    }
+  },
+  { urls: ["<all_urls>"] }
+);
+
+document.addEventListener("DOMContentLoaded", () => {
+  browserApi.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    browserApi.scripting.executeScript(
+      {
+        target: { tabId: tabs[0].id },
+        function: checkAutoClickStatus,
+      },
+      (results) => {
+        if (browserApi.runtime.lastError || !results || !results[0].result) {
+          // Nếu có lỗi hoặc không thể lấy kết quả, mặc định là "chưa chạy"
+          document.getElementById("start-auto-click").style.display = "inline";
+          document.getElementById("stop-auto-click").style.display = "none";
+        } else {
+          // Cập nhật nút dựa trên trạng thái của autoClickRunning
+          if (results[0].result) {
+            document.getElementById("start-auto-click").style.display = "none";
+            document.getElementById("stop-auto-click").style.display = "inline";
+          } else {
+            document.getElementById("start-auto-click").style.display =
+              "inline";
+            document.getElementById("stop-auto-click").style.display = "none";
+          }
+        }
+      }
+    );
+  });
 });
 
 browserApi.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab.url && tab.url.includes("https://poll.fm/14165530/embed")) {
-        browserApi.storage.local.get('autoClickRunning', (data) => {
-            if (data.autoClickRunning) {
-                browserApi.scripting.executeScript({
-                    target: { tabId: tabId },
-                    function: startAutoClickVote,
-                    args: [5, 2000, 60000]
-                });
-            }
+  if (
+    changeInfo.status === "complete" &&
+    tab.url &&
+    tab.url.includes("https://poll.fm/14276245/embed")
+  ) {
+    browserApi.storage.local.get("autoClickRunning", (data) => {
+      if (data.autoClickRunning) {
+        browserApi.scripting.executeScript({
+          target: { tabId: tabId },
+          function: startAutoClickVote,
+          args: [5, 2000, 60000],
         });
-    }
+      }
+    });
+  }
 });
 
-document.getElementById('start-auto-click').addEventListener('click', () => {
-    browserApi.storage.local.set({ clickCount: 0 });
-    browserApi.storage.local.set({ totalClickCount: 0 });
-    browserApi.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        browserApi.scripting.executeScript({
-            target: { tabId: tabs[0].id },
-            function: startAutoClickVote,
-            args: [5, 2000, 60000]
-        });
+document.getElementById("start-auto-click").addEventListener("click", () => {
+  browserApi.storage.local.set({ clickCount: 0 });
+  browserApi.storage.local.set({ totalClickCount: 0 });
+  browserApi.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    browserApi.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      function: startAutoClickVote,
+      args: [5, 2000, 60000],
     });
+  });
 
-    browserApi.storage.local.set({ autoClickRunning: true });
+  browserApi.storage.local.set({ autoClickRunning: true });
 
-    document.getElementById('start-auto-click').style.display = 'none';
-    document.getElementById('stop-auto-click').style.display = 'inline';
+  document.getElementById("start-auto-click").style.display = "none";
+  document.getElementById("stop-auto-click").style.display = "inline";
 });
 
-document.getElementById('stop-auto-click').addEventListener('click', () => {
-    browserApi.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        browserApi.scripting.executeScript({
-            target: { tabId: tabs[0].id },
-            function: stopAutoClickVote
-        });
+document.getElementById("stop-auto-click").addEventListener("click", () => {
+  browserApi.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    browserApi.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      function: stopAutoClickVote,
     });
+  });
 
-    browserApi.storage.local.set({ autoClickRunning: false });
+  browserApi.storage.local.set({ autoClickRunning: false });
 
-    document.getElementById('start-auto-click').style.display = 'inline';
-    document.getElementById('stop-auto-click').style.display = 'none';
+  document.getElementById("start-auto-click").style.display = "inline";
+  document.getElementById("stop-auto-click").style.display = "none";
 });
 
 function checkAutoClickStatus() {
-    return new Promise((resolve) => {
-        getBrowserApi().storage.local.get('autoClickRunning', (data) => {
-            resolve(data.autoClickRunning || false);
-        });
+  return new Promise((resolve) => {
+    getBrowserApi().storage.local.get("autoClickRunning", (data) => {
+      resolve(data.autoClickRunning || false);
     });
+  });
 }
